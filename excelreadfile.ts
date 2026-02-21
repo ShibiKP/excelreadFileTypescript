e1c4b7b2d2439d6a80c9b777c3cf1889703df0c7
shibi kp <kkpshibi@gmail.com>
2026-02-21 19:46:41 +0530

    update code

--- excelreadfile.ts
+++ excelreadfile.ts
@@ -1,15 +1,28 @@
 import * as ExcelJS from 'exceljs'
-
-const workbook = new ExcelJS.Workbook
-
+const workbook=new ExcelJS.Workbook()
 workbook.xlsx.readFile('D:/download.xlsx').then(
-    function () {
-        const sheet = workbook.getWorksheet('Sheet1')
-
-        sheet?.eachRow((row, rownumber) =>
-            row.eachCell((cell, cellnumber) =>
-                console.log(cell.value)
+    function(){
+        const sheet=workbook.getWorksheet('Sheet1')
+        sheet?.eachRow((row,rownumber)=>
+            row.eachCell((cell,coloumnumber)=>
+                console.log(cell.value)            
             )
         )
     }
 )
+
+// Another option to read the file
+
+async function excelread(){
+console.log('----------------------****************-------------')
+
+   await  workbook.xlsx.readFile('D:/download.xlsx')
+   const sheet= workbook.getWorksheet();
+   sheet?.eachRow((row,rownumber)=>
+    row.eachCell((cell,coloumnumber)=>
+        console.log('****__'+cell.value)
+    )
+)
+}
+
+excelread()
